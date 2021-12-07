56e6f87fbbf72f68941fe732dd8bb92f37a101cc
tangyw <tangyw@landray.com.cn>
2021-12-07 13:54:21 +0800

    自定义mapper方式

--- IPersistence_test/src/test/java/com/test/IPersistenceTest.java
+++ IPersistence_test/src/test/java/com/test/IPersistenceTest.java
@@ -1,10 +1,14 @@
 package com.test;
 
 import com.test.dao.IUserDao;
-import com.test.dao.UserDaoImpl;
+import com.test.io.Resources;
 import com.test.pojo.User;
+import com.test.sqlsession.SqlSession;
+import com.test.sqlsession.SqlSessionFactory;
+import com.test.sqlsession.SqlSessionFactoryBuilder;
 import org.junit.Test;
 
+import java.io.InputStream;
 import java.util.List;
 
 /**
@@ -16,39 +20,23 @@ public class IPersistenceTest {
 
     @Test
     public void test() throws Exception {
-        // 这里使用UserMapper2.xml的配置方法
-        IUserDao userDao = new UserDaoImpl();
+        InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
 
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        IUserDao userDao = sqlSession.getMapper(IUserDao.class);
         User user = new User();
-        user.setId(4);
-        user.setUsername("小王");
+        user.setId(1);
+        user.setUsername("唐有炜");
+
         User user2 = userDao.findByCondition(user);
         System.out.println(user2);
 
-        System.out.println();
-
         List<User> users = userDao.findAll();
+
         for (User user3 : users) {
             System.out.println(user3);
         }
-
-//        InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
-//        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
-//
-//        SqlSession sqlSession = sqlSessionFactory.openSession();
-//
-//        IUserDao userDao = sqlSession.getMapper(IUserDao.class);
-////        User user = new User();
-////        user.setId(1);
-////        user.setUsername("唐有炜");
-////
-////        User user2 = userDao.findByCondition(user);
-////        System.out.println(user2);
-//
-//        List<User> users = userDao.findAll();
-//
-//        for (User user3 : users) {
-//            System.out.println(user3);
-//        }
     }
 }

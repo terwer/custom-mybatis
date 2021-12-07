2e832cb8cd4268b31c9b31b91ece8bee5d63f3ea
tangyw <tangyw@landray.com.cn>
2021-12-07 14:26:01 +0800

    传统方式

--- IPersistence_test/src/test/java/com/test/IPersistenceTest.java
+++ IPersistence_test/src/test/java/com/test/IPersistenceTest.java
@@ -16,39 +16,25 @@ public class IPersistenceTest {
 
     @Test
     public void test() throws Exception {
-        // 这里使用UserMapper2.xml的配置方法
         IUserDao userDao = new UserDaoImpl();
 
         User user = new User();
-        user.setId(4);
-        user.setUsername("小王");
+        user.setId(2);
+        user.setUsername("张月");
         User user2 = userDao.findByCondition(user);
         System.out.println(user2);
 
         System.out.println();
 
+    }
+
+    @Test
+    public void test2() throws Exception {
+        IUserDao userDao = new UserDaoImpl();
+
         List<User> users = userDao.findAll();
         for (User user3 : users) {
             System.out.println(user3);
         }
-
-//        InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
-//        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
-//
-//        SqlSession sqlSession = sqlSessionFactory.openSession();
-//
-//        IUserDao userDao = sqlSession.getMapper(IUserDao.class);
-////        User user = new User();
-////        user.setId(1);
-////        user.setUsername("唐有炜");
-////
-////        User user2 = userDao.findByCondition(user);
-////        System.out.println(user2);
-//
-//        List<User> users = userDao.findAll();
-//
-//        for (User user3 : users) {
-//            System.out.println(user3);
-//        }
     }
 }
